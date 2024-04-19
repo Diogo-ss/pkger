0053565f99767b5f16887e8d491887c45cd46d92
Diogo-ss <diogosantosdasilva6@gmail.com>
2024-04-19 19:27:03 -0300

    chore: update script

--- scripts/scripts/build.sh
+++ scripts/scripts/build.sh
@@ -8,6 +8,8 @@ set -eu
 # luarocks install luastatic
 # luastatic main.lua $(brew --prefix lua)/lib/liblua.a -I$(brew --prefix lua)/include/lua
 
+echo -n "Version: " && grep -o 'PKGER_VERSION = "[^"]*"' src/core/global.lua | cut -d '"' -f 2
+
 cd src
 
 luastatic init.lua $(find . -type f -name "*.lua") $(brew --prefix lua)/lib/liblua.a -I$(brew --prefix lua)/include/lua -o pkger
