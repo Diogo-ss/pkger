860663c4beabaf0da0ee1411a7e712457bbfb93f
Diogo-ss <diogosantosdasilva6@gmail.com>
2024-05-11 23:06:00 -0300

    feat: use unlink and link in switch

--- src/commands/switch/init.lua
+++ src/commands/switch/init.lua
@@ -5,6 +5,8 @@ local fn = require "src.utils.fn"
 local lfs = require "lfs"
 local fs = require "src.utils.fs"
 local c = require "src.utils.colors"
+local L = require "src.commands.link"
+local U = require "src.commands.unlink"
 
 local M = {}
 
@@ -16,31 +18,17 @@ function M.switch(name, version)
     return
   end
 
-  local new_pkg = lpkg.get_pkg_infos(name, version)
-
-  if not new_pkg then
-    log.err(("version %s is not installed"):format(c.cyan(version)))
+  if not lpkg.get_pkg_infos(name, version) then
+    log.err(("Version %s is not installed"):format(c.cyan(version)))
   end
 
-  if current_pkg then
-    local path = fs.join(PKGER_BIN, current_pkg.bin_name)
-
-    if fs.is_file(path) and not fs.rm(path) then
-      log.err("It was not possible to remove the symbolic link: " .. path)
-    end
-  end
-
-  new_pkg.INSTALLATION_DIRECTORY = new_pkg.dir
-
-  lpkg.gen_pkg_file(new_pkg)
-  lpkg.create_link(new_pkg)
+  U.unlink(name)
+  L.link(name, version, { overwrite = true })
 
   if current_pkg and current_pkg.version then
     log.info(("%s: %s --> %s"):format(name, current_pkg.version, version))
     return
   end
-
-  log.info(("Complete, %s@%s is available."):format(name, version))
 end
 
 function M.parser(args)
