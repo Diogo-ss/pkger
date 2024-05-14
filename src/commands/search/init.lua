28fa9bdbdcf5dda67b015266d5593d9fdf1ccf7d
Diogo-ss <diogosantosdasilva6@gmail.com>
2024-05-14 18:34:16 -0300

    feat: refactor search

--- src/commands/search/init.lua
+++ src/commands/search/init.lua
@@ -6,7 +6,7 @@ local fn = require "src.utils.fn"
 
 local M = {}
 
-function M.show(results)
+local function show(results)
   for _, value in pairs(results) do
     log(c.cyan(value.engine) .. ": " .. c.yellow(value.repo))
     for name, versions in pairs(value.pkgs) do
@@ -16,7 +16,7 @@ function M.show(results)
   end
 end
 
-local function search(url, engine_type, name)
+local function _find(url, engine_type, name)
   url = url:gsub("%${{%sname%s}}", name)
   local ok, engine = pcall(require, "src.commands.search." .. engine_type:lower())
 
@@ -45,12 +45,11 @@ function M.find(name)
   end)
 
   if tbl.is_empty(repos) then
-    log.error "No repository with search support was found."
-    fn.exit(1)
+    log.err "No repository with search support was found."
   end
 
   for _, repo_search in pairs(repos) do
-    local result = search(repo_search.url, repo_search.type, name)
+    local result = _find(repo_search.url, repo_search.type, name)
 
     if results then
       table.insert(results, result)
@@ -60,20 +59,24 @@ function M.find(name)
   return results
 end
 
-function M.parser(args)
+function M.search(name, flags)
+  local results = M.find(name)
+
+  if tbl.is_empty(results) then
+    log.warn("No packages found for: " .. name)
+    return
+  end
+
+  show(results)
+end
+
+function M.parser(args, flags)
   if #args ~= 1 then
     log.warn "Search for a single package."
     return
   end
 
-  local results = M.find(args[1])
-
-  if tbl.is_empty(results) then
-    log.warn("No packages found for: " .. args[1])
-    fn.exit(1)
-  end
-
-  M.show(results)
+  local ok, _ = pcall(M.search, args[1], flags)
 end
 
 return M
