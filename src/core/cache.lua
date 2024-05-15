927e90b7d1980cb10aa4560200802443bdb3bf67
Diogo-ss <diogosantosdasilva6@gmail.com>
2024-05-14 21:36:58 -0300

    feat: new cache with db

--- src/core/cache.lua
+++ src/core/cache.lua
@@ -5,9 +5,15 @@ local log = require "src.utils.log"
 local config = require "src.core.config"
 
 local M = {}
-local limit = 3600
 
-function M.save(name, cache)
+function M:new()
+  return setmetatable({
+    limit = 3600,
+    db = {},
+  }, { __index = self })
+end
+
+function M:save(name, cache)
   local C = {
     timestamp = os.time(),
     cache = cache,
@@ -15,32 +21,50 @@ function M.save(name, cache)
 
   local text = "return " .. fn.inspect(C)
   local file = fs.join(PKGER_CACHE, name)
+
+  self.db[name] = cache
+
   return fs.write_file(file, text)
 end
 
-function M.load(name, flags)
+function M:load(name, flags)
+  if self.db[name] and (os.time() - (self.db[name].timestamp or 0) < self.limit) then
+    return self.db[name].cache
+  end
+
   local file = fs.join(PKGER_CACHE, name)
   local ok, C = pcall(dofile, file)
 
-  if ok and (os.time() - (C.timestamp or 0) < limit) then
+  if ok and (os.time() - (C.timestamp or 0) < self.limit) then
+    self.db[name] = C
     return C.cache
   end
 
   return nil
 end
 
-function M.clear(name)
+function M:clear(name)
   local file = fs.join(PKGER_CACHE, name or "")
 
-  if name and fs.is_file(file) then
-    return fs.rm(file)
+  if name then
+    self.db[name] = nil
+    if fs.is_file(file) then
+      return fs.rm(file)
+    end
   end
 
-  if not name and fs.is_dir(PKGER_CACHE) then
-    return fs.rm_dir(PKGER_CACHE)
+  if not name then
+    self.db = {}
+    if fs.is_dir(PKGER_CACHE) then
+      return fs.rm_dir(PKGER_CACHE)
+    end
   end
 
   return false, nil
 end
+
+-- function M:print()
+--   print(self.limit)
+-- end
 
 return M
