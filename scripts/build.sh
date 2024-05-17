a4dbbd94ac04310f43988c23404a3d9b79c9a30e
Diogo Silva <86174715+Diogo-ss@users.noreply.github.com>
2024-05-16 21:37:31 -0300

    ci: add new build

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,22 +5,23 @@ set -eu
 echo -n "Version: " && grep -o 'PKGER_VERSION = "[^"]*"' src/core/global.lua | cut -d '"' -f 2
 
 if ! command -v brew &>/dev/null; then
-    echo "Homebrew is not installed. Installing..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "Homebrew is not installed."
+    exit 1
+    # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 if ! command -v luarocks &>/dev/null; then
-    echo "Luarocks is not installed. Installing..."
-    brew install lua luarocks
+    echo "Luarocks is not installed."
+    exit 1
 fi
 
 if ! command -v luastatic &>/dev/null; then
-    luarocks install luastatic
+    if ! luarocks install luastatic; then
+        echo "Failed to install luastatic."
+        exit 1
+    fi
 fi
 
-cd src
+SRC_DIR="src"
 
-luastatic main.lua $(find . -type f -name "*.lua") $(brew --prefix lua)/lib/liblua.a -I$(brew --prefix lua)/include/lua -o pkger
-
-rm main.luastatic.c
-mv pkger ../
+luastatic "$SRC_DIR/main.lua" $(find "$SRC_DIR" -type f -name "*.lua") "$(brew --prefix lua)/lib/liblua.a" -I"$(brew --prefix lua)/include/lua" -o "$SRC_DIR/pkger"
