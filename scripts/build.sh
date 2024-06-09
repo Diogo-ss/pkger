a0dc21f0bc544035ae467707598bcf9f6e3229c9
Diogo-ss <ssdiogo@proton.me>
2024-06-09 09:57:05 -0300

    feat: add export luarocks path

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,6 @@
 
 set -eu
 
-echo -n "Version: " && grep -o 'PKGER_VERSION = "[^"]*"' src/core/global.lua | cut -d '"' -f 2
-
-if ! command -v brew &>/dev/null; then
-    echo "Homebrew is not installed."
-    exit 1
-    # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
 if ! command -v luarocks &>/dev/null; then
     echo "Luarocks is not installed."
     exit 1
@@ -22,6 +14,8 @@ if ! command -v luastatic &>/dev/null; then
     fi
 fi
 
-SRC_DIR="src"
+eval "$(luarocks path --bin)"
 
-luastatic "$SRC_DIR/main.lua" $(find "$SRC_DIR" -type f -name "*.lua") "$(brew --prefix lua)/lib/liblua.a" -I"$(brew --prefix lua)/include/lua" -o "pkger"
+# echo -n "Version: " && grep -o 'PKGER_VERSION = "[^"]*"' src/core/global.lua | cut -d '"' -f 2
+
+make
